fad25b16150e56ed593502cf22522f688532a69d
Razvan Jega <48220441+razvanjega@users.noreply.github.com>
2025-03-02 18:51:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,6 @@
-(function() {
-    $(".skills-prog li")
-      .find(".skills-bar")
-      .each(function(i) {
-        $(this)
-          .find(".bar")
-          .delay(i * 150)
-          .animate(
-            {
-              width:
-                $(this)
-                  .parents()
-                  .attr("data-percent") + "%"
-            },
-            1000,
-            "linear",
-            function() {
-              return $(this).css({
-                "transition-duration": ".5s"
-              });
-            }
-          );
-      });
-  
-    $(".skills-soft li")
-      .find("svg")
-      .each(function(i) {
-        var c, cbar, circle, percent, r;
-        circle = $(this).children(".cbar");
-        r = circle.attr("r");
-        c = Math.PI * (r * 2);
-        percent = $(this)
-          .parent()
+// This JavaScript file can be used for adding interactive features if needed
+// For now, it's just a placeholder for future extensions like form validation or animation effects
+
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("Resume website is loaded.");
+});
